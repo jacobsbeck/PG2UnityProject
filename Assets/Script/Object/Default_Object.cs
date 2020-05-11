b8c4817b5272ddef998e7d9c601b54e51fb99c2d
mcchow <44908668+mcchow@users.noreply.github.com>
2020-05-11 15:23:39 -0700

    Add files via upload

---
+++ Assets/Script/Object/Default_Object.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName ="ItemObject", menuName = "Inventory/Item/Default")]
+public class Default_Object : InventoryObject
+{
+    public void Awake()
+    {
+        type = ItemType.Default;
+    }
+}
---
+++ Assets/system.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class system : MonoBehaviour
+{
+    public Transform current_button = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Reset()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
