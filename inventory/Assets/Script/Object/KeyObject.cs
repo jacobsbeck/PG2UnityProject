1605fd46169994d7a780c5e34c22a027da944192
mcchow <44908668+mcchow@users.noreply.github.com>
2020-05-14 14:47:44 -0700

    This is the inventory trail
    This is the inventory trail

---
+++ inventory/Assets/Script/Object/KeyObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemObject", menuName = "Inventory/Item/Key")]
+
+public class KeyObject : InventoryObject
+{
+    public void Awake()
+    {
+        type = ItemType.Key;
+    }
+}
