09d1847547bf9b8c5ccd4b76f32045f19f01d3e8
SamuelAMunford <samuelamunford@gmail.com>
2020-05-17 22:52:26 -0700

    object dissapears
    -makes it so object only appears on certain screen

---
+++ InventoryV0.0.1/Assets/Script/Object/ObjectsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject blueWallKey;
+    private DisplayBackground currentBackground;
+
+    private void Start()
+    {
+        currentBackground = GameObject.Find("Background").GetComponent<DisplayBackground>();
+        blueWallKey = GameObject.Find("key");
+        blueWallKey.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentBackground.CurrentWall == 2)
+        {
+            blueWallKey.SetActive(true);
+        }
+        else
+        {
+            blueWallKey.SetActive(false);
+        }
+    }
+}
